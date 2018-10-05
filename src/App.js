b8f3c4138624fd12075ec56807867b0112201e13
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-10-05 16:30:09 -0500

    Estableciendo boilerplate

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//Key API 108bddcd9fefc3e86bc16a18ad53e089
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h2>
+            Ranking Songs
+            <img src={logo} className="App-logo" alt="logo" />
+          </h2>
+        </header>
+
+        
+      </div>
+    );
+  }
+}
+
+export default App;
