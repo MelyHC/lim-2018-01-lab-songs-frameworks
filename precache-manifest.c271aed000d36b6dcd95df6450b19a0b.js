6b16d0865fdc1bc5f952f137218d0319fb3f4082
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-10-09 15:27:51 -0500

    Updates

---
+++ precache-manifest.c271aed000d36b6dcd95df6450b19a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "45d975ffd74dd0f719d3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/runtime~main.45d975ff.js"
+  },
+  {
+    "revision": "5a95a0c9d6dc0e067e2c",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/main.5a95a0c9.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "5a95a0c9d6dc0e067e2c",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/css/main.8f34aa4f.chunk.css"
+  },
+  {
+    "revision": "35b3620aea0e593667eceebc9c631a16",
+    "url": "/lim-2018-01-lab-songs-frameworks/index.html"
+  }
+];
